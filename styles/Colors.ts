7e9bd0a0f6d1505b13d640ccd709fb1477a3a705
Marlleyck Nathan <marlleyck@hotmail.com>
2024-07-26 19:48:22 -0300

    feat: add app colors

---
+++ styles/Colors.ts
@@ -0,0 +1,12 @@
+const tintColorLight = '#0a7ea4';
+
+export const Colors = {
+    dark: {
+        text: '#11181C',
+        background: '#fff',
+        tint: tintColorLight,
+        icon: '#687076',
+        tabIconDefault: '#687076',
+        tabIconSelected: tintColorLight,
+      },
+}
