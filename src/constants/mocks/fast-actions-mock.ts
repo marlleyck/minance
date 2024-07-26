3c01110c189d6ea99be123c40d48b5d5c78197f7
Marlleyck Nathan <marlleyck@hotmail.com>
2024-07-26 20:47:29 -0300

    feat: add fast actions mock

---
+++ src/constants/mocks/fast-actions-mock.ts
@@ -0,0 +1,27 @@
+export const fastActionsMock = [
+  {
+    iconName: "arrow-up-long",
+    iconSize: 30,
+    title: "Receita",
+  },
+  {
+    iconName: "receipt",
+    iconSize: 30,
+    title: "Transações",
+  },
+  {
+    iconName: "arrow-down-long",
+    iconSize: 30,
+    title: "Despesa",
+  },
+  {
+    iconName: "calendar-times",
+    iconSize: 30,
+    title: "Débito Automático",
+  },
+  {
+    iconName: "credit-card",
+    iconSize: 30,
+    title: "Adicionar Cartão",
+  },
+];
