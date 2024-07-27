579f90bb0ed090026d7d4b24797b6c165e1474c2
Marlleyck Nathan <marlleyck@hotmail.com>
2024-07-26 22:35:58 -0300

    refactor: adjust card styles

--- src/components/layout/card/index.tsx
+++ src/components/layout/card/index.tsx
@@ -1,46 +1,65 @@
 import { Colors } from "@/styles/Colors";
-import { Text, View } from "react-native";
+import {
+  OpenSans_300Light,
+  OpenSans_500Medium,
+  OpenSans_700Bold,
+} from "@expo-google-fonts/open-sans";
+import { useFonts } from "expo-font";
+import { StyleSheet, Text, View } from "react-native";
 
 export function Card() {
+  const [loaded, error] = useFonts({
+    OpenSans_500Medium,
+    OpenSans_300Light,
+    OpenSans_700Bold,
+  });
+
+  if (!loaded) {
+    return <Text>...Loading</Text>;
+  }
+
   return (
-    <View
-      style={{
-        width: "100%",
-        backgroundColor: Colors.light.darkGreen,
-        height: 184,
-        borderRadius: 30,
-        alignItems: "center",
-        justifyContent: "space-between",
-        paddingHorizontal: 20,
-        paddingVertical: 30,
-      }}
-    >
-      <View
-        style={{
-          width: "100%",
-        }}
-      >
+    <View style={styles.container}>
+      <View style={styles.cardNumberContainer}>
         <Text
-          style={{
-            color: "white",
-          }}
+          style={[
+            styles.cardNumber,
+            {
+              fontFamily: "OpenSans_700Bold",
+            },
+          ]}
         >
           0000 0000 0000 0000
         </Text>
       </View>
-      <View
-        style={{
-          width: "100%",
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-          }}
-        >
-          Lorena K L Martins
-        </Text>
+      <View style={styles.cardNameContainer}>
+        <Text style={styles.cardName}>Lorena K L Martins</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    backgroundColor: Colors.light.darkGreen,
+    height: 184,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  cardNumberContainer: {
+    width: "100%",
+  },
+  cardNumber: {
+    color: "white",
+  },
+  cardNameContainer: {
+    width: "100%",
+  },
+  cardName: {
+    color: "white",
+  },
+});
