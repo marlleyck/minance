2e7d0160d1193c3c2decc6e4d23d33a66f4416f0
marlleyck <marlleyck@hotmail.com>
2024-11-16 16:29:47 -0300

    feat: create login screen

--- src/screens/auth/login/index.tsx
+++ src/screens/auth/login/index.tsx
@@ -1,9 +1,13 @@
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { Text } from "@/src/components/layout/text";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export function LoginScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <View></View>
+      <View>
+        <Text style={{ color: "black" }}>Login Screen</Text>
+      </View>
     </SafeAreaView>
   );
 }
